361c63fe5f369f223a9a9ba83a743bf476885d50
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-09-24 17:07:38 -0700

    [example] The example is now complete and works for this very repo

--- example/my_hook.py
+++ example/my_hook.py
@@ -1,10 +1,14 @@
 from lib.git import clone_tmp
-from lib.docker import docker_build
+from lib.docker import build, push
 
 ROUTE = '/push'
 PORT = 4242
 
+IMAGE_NAME = 'globidocker/github-hook'
+
 def on_push(data):
     url = data['repository']['html_url']
+
     with clone_tmp(url) as repo:
-        docker_build(repo.path, 'github-hook')
+        build(repo.path, IMAGE_NAME)
+        push(IMAGE_NAME)
