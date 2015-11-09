cd7bfca8777a8cd99a78545a1a007a2dedc6ada4
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-11-09 03:03:15 +0100

    Now executing the hook in a separate thread Also improved error handling a bit

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+import threading
+import traceback
+
+def call_hook(payload):
+    try:
+        hook.on_push(payload, app.logger)
+    except Exception:
+        app.logger.error('Error while executing hook')
+        app.logger.error(traceback.format_exc())
+
 @app.route(hook.ROUTE, methods=['POST'])
 def on_push():
     payload = request.get_json()
-    hook.on_push(payload)
+    hook_thread = threading.Thread(
+        target=call_hook,
+        args=(payload,)
+    )
+    hook_thread.start()
+
     return 'Ok!'
 
 if __name__ == '__main__':
